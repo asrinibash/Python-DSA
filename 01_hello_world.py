86cc3980a1ff1cc732676b55cc897b698c68a87a
asrinibash <asrinibashachary@gmail.com>
2025-07-30 01:20:15 +0530

    python print() and it params

---
+++ 01_hello_world.py
@@ -0,0 +1,14 @@
+## PYTHON FROM SCRATCH
+
+# print("hello World")
+
+print("india","python","Usa",5.5,"2",2,False)
+# india python Usa 5.5 2 2 False
+# in the output where is the space came from ? --> its due to 'sep' parameter - if we use ',' then it automatically take sep param
+
+print("india","python","Usa",5.5,"2",2,False,sep='/')
+# india/python/Usa/5.5/2/2/False --we can change the sep param value
+
+# end - this makes an new line. we can change explicitly its value
+print("hello",end=' ')
+print("world") #now it print in single line- no new line
