92c7e01fde7b8f0ac900eeb3377043876e4d34f9
asrinibash <asrinibashachary@gmail.com>
2025-08-03 16:30:36 +0530

    taking input

---
+++ 04_taking_input_and_type_conversion.py
@@ -0,0 +1,12 @@
+# Taking User Input
+
+# input()
+first_num=input("Enter your first num: ") # it takes a argument called "prompt" where we can prompt to user
+second_num=input("Enter your second num:")
+
+result=first_num+second_num
+print(result)
+# if we pass number, the input() takes it as a string only because for generic input
+
+# if get integer from user we need to do type conversion
+# what ever we are accepting from user input in python is in string type only.
