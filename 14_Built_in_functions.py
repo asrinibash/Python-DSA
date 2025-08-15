fde720b7b3b9b97a62e302fd8efa7d66eab21fad
asrinibash <asrinibashachary@gmail.com>
2025-08-16 04:17:25 +0530

    Some important built-in functions of python used in daily basis

---
+++ 14_Built_in_functions.py
@@ -0,0 +1,33 @@
+# Some important built-in functions of python used in daily basis
+
+# 1. print() -> use for print values
+
+# 2. input() -> used for get user input
+
+# 3. type() -> used get data type
+
+# 4. int() -> type conversion like many type conversion functions are there
+
+# 5. abs() -> for printing absolute value
+
+# 6. pow() -> for doing power calculations
+
+# 7. min()/max() -> for getting minimum or maximum value
+    # it can used with string values also like max("Odisha") -> it will print s cuz, s has higher ascii value
+    
+# 8. round() -> for reducing decimal values digits
+
+# 9. divmod() -> for getting both division value and modulus value of given number
+
+# 10. bin()/oct()/hex() -> for getting binary or octal or hexadecimal value of a given number
+
+# 11. id() -> for getting memory address of a given number
+
+# 12. ord() -> for getting ascii value of string value
+
+# 13. len() -> for getting length of string or list,tuple,set etc
+
+# 14. sum() -> for calculating sum of iterable objects like list,tuple,set etc
+
+# 15. help('print') -> for getting of documentation of the given function or keywords etc
+
