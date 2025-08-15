bbe4aeb14216f883a96b183f87c08592f3c043a6
asrinibash <asrinibashachary@gmail.com>
2025-08-16 03:07:47 +0530

    While loops in python

---
+++ 09_Loops.py
@@ -0,0 +1,13 @@
+# Loops in Python:
+
+# While Loop:
+
+# While condition:
+#     code
+
+# Ex: Multiplication Table
+number=int(input("Enter the number: "))
+i=1
+while i<=10:
+    print(number,"*",i,"=",number*i)
+    i+=1
