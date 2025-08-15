ec52dc06023f78edd6a569873d4418dafdbefb8a
asrinibash <asrinibashachary@gmail.com>
2025-08-16 03:32:41 +0530

    For loop in python

---
+++ 11_for-loop.py
@@ -0,0 +1,31 @@
+# For-Loop:
+
+# Before jump into for loop we need to know about 
+
+# range() -> it takes 3 args start=0,end,step=1(default value)
+# range(10) -> if we give single value then it takes as end
+
+# print(list(range(10)))
+
+# Syntax Example:
+
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(1,10,2):   # instead of increment we can use step param
+#     print(i)
+
+for i in "Kolkata":
+    print(i) 
+    
+#Output :
+# K
+# o
+# l
+# k
+# a
+# t
+# a 
+
+
+# ** We can run the for loop in range and sequence(list,tuple,set,string,dict also)
