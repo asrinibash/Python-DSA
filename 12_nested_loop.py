0e28aaa49ec368a579660d7aa1d8fc51ebe6516a
asrinibash <asrinibashachary@gmail.com>
2025-08-16 03:44:05 +0530

    nested loop in python

---
+++ 12_nested_loop.py
@@ -0,0 +1,14 @@
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# for print this we have to use nested loop - nested loop are mainly called as loop inside loop
+
+# Code:
+for i in range(1,6):
+    for j in range(0,i):
+        print("*",end=" ")
+    print("")
+    
