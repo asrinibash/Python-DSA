a6e70fe645335e5baeca42c01c55a33c45c98e80
asrinibash <asrinibashachary@gmail.com>
2025-08-16 02:28:35 +0530

    indentation in python

---
+++ 08_indentation.py
@@ -0,0 +1,11 @@
+# Indentation : its space given for represent block of code.
+# Unlike other languages python doesn't have any moon bracket of curly braces for represent block of code.
+
+# don't use space to give indentation use tab( because it needs a tab of space)
+
+a=0
+
+if a==0:
+    print("Hello") # the space before the print start is called indentation. 
+else:
+    print("World")
