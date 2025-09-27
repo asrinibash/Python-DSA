ee89639d885be3bd95e7109a46a25bc8b1455699
asrinibash <asrinibashachary@gmail.com>
2025-09-27 23:15:09 +0530

    convert string of characters to decimal

--- python_operations/01_convert_int_decimal.py
+++ python_operations/01_convert_int_decimal.py
@@ -7,3 +7,12 @@ print(decimal.Decimal(inter))
 print(type(decimal.Decimal(inter)))
 
 # by importing the decimal package we can convert integer to decimal
+
+
+
+# Converting String of integers into decimal
+
+strin="1234"
+
+print(decimal.Decimal(strin))
+print(type(decimal.Decimal(strin)))
